fb254c7b5471d0d711ece4dccfbb8eb4fb93792a
Caroline Lee <carolineL@hzdg.com>
2019-12-27 05:22:43 -0500

    before anything else goes wrong

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,8 +7,20 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <div>
+    <h1>Our Wedding Party</h1>
+    </div>
+
+    <div>
+      <h1>Groomsmen</h1>
+      <p>We look forward to seeing you! In order to manage the guest count, we have reserved additional guest spots for couples that are engaged or married only. If you have specific dietary restrictions, please make sure it’s noted below. </p>
+    </div>
+
+    <div>
+      <h1>Bridesmaids</h1>
+      <p>We look forward to seeing you! In order to manage the guest count, we have reserved additional guest spots for couples that are engaged or married only. If you have specific dietary restrictions, please make sure it’s noted below. </p>
+    </div>
+
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
