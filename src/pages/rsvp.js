d271571c935fc7249d54afc480118ec45b2ed54e
Caroline Lee <carolineL@hzdg.com>
2019-12-27 19:48:59 -0500

    updated form to look better

--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -18,20 +18,20 @@ const SecondPage = () => (
             <input type="hidden" name="bot-field" />
             <input type="hidden" name="form-name" value="response" />
             
-            <div className="field half first">
+            <StyledInputContainer className="field half first">
                 <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" />
-            </div>
+                <StyledInput type="text" name="name" id="name" />
+            </StyledInputContainer>
 
-            <div className="field half">
+            <StyledInputContainer className="field half">
                 <label htmlFor="email">Email</label>
-                <input type="text" name="email" id="email" />
-            </div>
+                <StyledInput type="text" name="email" id="email" />
+            </StyledInputContainer>
 
-            <div className="field">
+            <StyledInputContainer className="field">
                 <label htmlFor="message">Message</label>
-                <textarea name="message" id="message" rows="6" />
-            </div>
+                <StyledTextbox name="message" id="message" rows="6" />
+            </StyledInputContainer>
 
             <div className="actions">
                 <input type="submit" value="Send Message" className="special" />
@@ -58,6 +58,11 @@ const ContentContainer = styled.div`
     max-width: 700px;
     text-align: center;
     font-family: Calibre-Light;
+
+    ${lessThan.tablet} {
+        max-width: 370px;
+    }
+
 `
 
 const Title = styled.h1`
@@ -69,6 +74,33 @@ const Title = styled.h1`
 const Copy = styled.p`
     ${lessThan.tablet} {
         font-size: 20px;
-        padding: 0 35px;
+        padding: 0px;
     }
+`
+
+const StyledInput = styled.input`
+    height: 45px;
+    width: 100%;
+    border-radius: 10px;
+    border-color: white;
+    padding-left: 10px;
+    border: none;
+
+
+`
+
+
+const StyledTextbox = styled.textarea`
+    height: 150px;
+    width: 100%;
+    border-radius: 10px;
+    border-color: white;
+    padding-left: 10px;
+    border: none;
+`
+
+const StyledInputContainer = styled.div`
+    margin-top: 20px;
+    position: relative;
+    text-align: left;
 `
