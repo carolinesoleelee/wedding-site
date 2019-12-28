f9ff88577fa79c2f00ace6be3ecaac9da67c6723
Caroline Lee <carolineL@hzdg.com>
2019-12-27 19:31:31 -0500

    will it still work?

--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -14,30 +14,29 @@ const SecondPage = () => (
     <Copy>We look forward to seeing you! In order to manage the guest count, we have reserved additional guest spots for couples that are engaged or married only. If you have specific dietary restrictions, please make sure it’s noted below. </Copy>
     <Copy><strong>Please RSVP by January 20, 2020</strong></Copy>
 
-    <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-  <input type="hidden" name="bot-field" />
-  <input type="hidden" name="form-name" value="contact" />
-  <div className="field half first">
-    <label htmlFor="name">Name</label>
-    <input type="text" name="name" id="name" />
-  </div>
-  <div className="field half">
-    <label htmlFor="email">Email</label>
-    <input type="text" name="email" id="email" />
-  </div>
-  <div className="field">
-    <label htmlFor="message">Message</label>
-    <textarea name="message" id="message" rows="6" />
-  </div>
-  <ul className="actions">
-    <li>
-      <input type="submit" value="Send Message" className="special" />
-    </li>
-    <li>
-      <input type="reset" value="Clear" />
-    </li>
-  </ul>
-</form>
+    <form name="RSVP Response" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="response" />
+            
+            <div className="field half first">
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" />
+            </div>
+
+            <div className="field half">
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" id="email" />
+            </div>
+
+            <div className="field">
+                <label htmlFor="message">Message</label>
+                <textarea name="message" id="message" rows="6" />
+            </div>
+
+            <div className="actions">
+                <input type="submit" value="Send Message" className="special" />
+            </div>
+    </form>
 
 
 
